df5f28340b0f28b706097e362a6210b7542aabde
João Lacerda <105456170+jf-lacerda@users.noreply.github.com>
2023-01-08 23:31:23 -0300

    number

--- tipagemFraca.js
+++ tipagemFraca.js
@@ -1,3 +1,9 @@
 let qualquer = 'Legal'
 console.log(qualquer)
 console.log(typeof qualquer)
+
+
+// Evitar nome genéricos e siglas
+let valor = ''
+let numero = ''
+let pqp = false // Produtos químos perigoso
