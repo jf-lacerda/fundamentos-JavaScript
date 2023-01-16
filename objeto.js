522786698bc796edf57b46c1de306ff0a101eef7
João Lacerda <joao.sclf@gmail.com>
2023-01-16 20:58:17 -0300

    null-undefined

---
+++ objeto.js
@@ -0,0 +1,16 @@
+const prod1 = {}
+prod1.nome = 'Celular ultra mega'
+prod1.preco = 4999.90
+prod1['Desconco legal'] = 0.50
+
+console.log(prod1)
+
+
+const prod2 = {
+    nome: 'Bermuda',
+    preco: 75.90,
+    tamanho: 'M',
+    cor: 'Preto'
+}
+
+console.log(prod2)
