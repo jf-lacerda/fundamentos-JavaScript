9c9ea1d7aa17892d87908d350f0231e24cd1b046
João Lacerda <joao.sclf@gmail.com>
2023-02-02 22:09:02 -0300

    unarios

---
+++ unarios.js
@@ -0,0 +1,13 @@
+let num1 = 1
+let num2 = 2
+
+num1++ //pós fixada - adiciona 1
+
+console.log(num1, num2)
+
+--num1 //pré-fixada  subtrai 1
+
+console.log(num1, num2)
+
+console.log(++num1 === num2--)
+console.log(num1 === num2)
