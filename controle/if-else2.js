a0a317e4fbfce63ee285667003dffcb5de540e34
João Lacerda <joao.sclf@gmail.com>
2023-02-12 23:52:00 -0300

    ifelseif

---
+++ controle/if-else2.js
@@ -0,0 +1,13 @@
+const pontuacao = function(pontos) {
+    if(pontos >= 45) {
+        console.log('Aprovado Fica na Série A')
+    }
+    if(pontos >= 50) {
+        console('Fica na seria A e se classifica para a Sul-Americada')
+    }
+    else { (pontos <= 45) 
+        console.log('Rebaixado') 
+    } 
+}
+
+pontuacao(50)
