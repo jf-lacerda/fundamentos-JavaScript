3bf469a60e78a1fa1c978d14fbcf4e38ccf54dd7
João Lacerda <joao.sclf@gmail.com>
2023-02-15 23:54:42 -0300

    for1

---
+++ controle/for1.js
@@ -0,0 +1,13 @@
+let contador = 1
+while(contador <= 10){
+    console.log(`contador = ${contador}`)
+    contador++
+}
+
+//declaração, expressão e incremento estão presentes no laço FOR
+
+for(let i = 1; i <= 10; i++) {
+    console.log(`i = ${i}`)
+}
+
+const notas = [6.7, 5.9, 4.6, 4.9, 7.7]
